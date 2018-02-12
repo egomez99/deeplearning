8049ca9371480263d1e0647424068b92e360e0da
Unknown <fernandovs4@gmail.com>
2018-02-12 10:02:08 -0600

    Entrenamientos iniciales
    [X] se agregan los archivos resultado del entrenamiento
    [X] se termina de entrenar la red

--- Proyecto #3/kerasp.py
+++ Proyecto #3/kerasp.py
@@ -3,30 +3,10 @@ from keras.models import Sequential, load_model
 from keras.layers.core import Dense,Activation,Dropout
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-def step_activation(x):
-	return 0 if x < 0 else 1
-
-
-def training(x_train,y_train,weights,iterations,learning_rate):
-	for it in range(iterations):
-		for shape in range(len(x_train)):
-				image =x_train[shape]
-				#print("shape w:{} , shape x:{}".format(weights.shape,image.shape))
-				result=np.dot(weights[shape],image)
-				print("w:{} \n\n image:{} \n\n result:{}".format(weights[shape],image,result))
-				print(y_train[shape])
-				error=y_train[shape]-step_activation(result)
-				weights[shape]+=learning_rate*error*image
-				print("result:{}, error:{}, w:{}".format(result,error,weights[shape]))
-
-def training2(x_train,y_train,weights,iterations,learning_rate):
-	for it in range(iterations):
-		result=np.dot(weights,x_train.T)
-		error=y_train[shape]-step_activation(result)
-		print(result)
-
-ITERATION_STEPS=1000
+EPOCHS=1000
+batch_size=128
 
 (X_train,Y_train),(X_test,Y_test)=mnist.load_data()
 
@@ -36,13 +16,7 @@ np.random.shuffle(indexes)
 
 weights=np.random.rand(784)
 
-print("shape w:{} shape x:{}".format(weights.shape,X_train.shape))
 
-#X_train=np.reshape(X_train,(len(X_train),784))
-#print("shape w:{} shape x:{}".format(weights.shape,X_train.shape))
-# print("Training Len:",len(X_train))
-# print("Weights Array: ",weights)
-# print("Weights Len: ",len(weights))
 for shape in range(16):
 	index=indexes[shape]
 	plt.subplot(4,4,shape+1)
@@ -51,28 +25,53 @@ for shape in range(16):
 	plt.title("Valor: {}".format(Y_train[index]))
 	plt.xticks([])
 	plt.yticks([])
-plt.show()
+#plt.show()
 
 X_train=X_train.reshape(len(X_train),784)
 X_train = X_train.astype('float32')
 X_train/=255
 
+X_test=X_test.reshape(len(X_test),784)
+X_test = X_test.astype('float32')
+X_test/=255
+
 
 model =Sequential()
 model.add(Dense(512,input_shape=(784,)))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(Dropout(0.2))
 
 model.add(Dense(512))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(512))
+model.add(Activation('relu'))
 model.add(Dropout(0.2))
 
 model.add(Dense(10))
-model.add(Activation('sigmoid'))
-model.compile(loss='mean_squared_error',optimizer='sgd')
+model.add(Activation('relu'))
 
-print("weights:{}".format(model.trainable_weights))
+model.add(Dense(10))
+model.add(Activation('relu'))
 
+model.add(Dense(1))
+model.add(Activation('relu'))
 
-#training2(X_train,Y_train,weights,ITERATION_STEPS,.02)
-#training(X_train,Y_train,weights,ITERATION_STEPS,.02)
+model.compile(loss='mean_squared_error', metrics=['accuracy'],optimizer='sgd')
+
+model.fit(X_train, Y_train,
+          batch_size=batch_size,
+          epochs=EPOCHS,
+          verbose=1,
+          validation_data=(X_test, Y_test))
+
+# saving the model
+model_name = 'keras_model_1000.h5'
+model_weights='mnist_weight_1000.h5'
+model.save(model_name)
+model.save_weights(model_weights)
+print('Saved trained model at %s ' % model_name)
+
+score = model.evaluate(X_test, Y_test, verbose=1)
+print('Test loss:', score)
